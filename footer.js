56c756b8e77924b38b1b1446d954910c21857a35
Caio Norder <caio@caionorder.com>
2016-07-05 14:05:58 -0300

    new version footer

--- footer.js
+++
@@ -1,2 +0,0 @@
-var url = 'https://www.iconeinternet.com.br/plugin/footer/';
-document.getElementById('development').innerHTML = '<iframe src='+url+' width="100%" height="100px" border="0" style="border:0;">';
